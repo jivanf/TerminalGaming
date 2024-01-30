37e00edf127d58a72b6283d6d89360997be532cc
jivanf <jivan.flores.108@gmail.com>
2024-01-29 22:59:55 -0800

    One day, or, day one

---
+++ TerminalGaming/Plugin.cs
@@ -0,0 +1,13 @@
+﻿using BepInEx;
+
+namespace TerminalGaming
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            this.Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
